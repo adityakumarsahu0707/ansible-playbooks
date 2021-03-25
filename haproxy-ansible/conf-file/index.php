8538f7a71a7de80347441b35683b4ba7fe055d54
Aditya Kumar Sahu <adityakumarsahu45@gmail.com>
2021-03-25 13:35:19 +0530

    haproxy branch

---
+++ haproxy-ansible/conf-file/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head> <title> Httpd Webserver </title> </head>
+	<body bgcolor="yellow">
+		<h1> Hello guys it's Ansible Automation </h1>
+		<h2> Proxy server: </h2>
+		<?php
+   			$ip = shell_exec("/sbin/ifconfig enp0s3");
+   			echo "<h1><pre>$ip</pre></h1>";
+		?>
+</body>
+</html>
