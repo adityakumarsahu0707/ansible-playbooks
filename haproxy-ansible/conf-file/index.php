c4939e593efacb0a8b4c5e4504a3c18d8981d95d
Aditya Kumar Sahu <adityakumarsahu45@gmail.com>
2021-03-26 14:09:13 +0530

    complete fetching ip

--- haproxy-ansible/conf-file/index.php
+++ haproxy-ansible/conf-file/index.php
@@ -4,7 +4,7 @@
 		<h1> Hello guys it's Ansible Automation </h1>
 		<h2> Proxy server: </h2>
 		<?php
-   			$ip = shell_exec("/sbin/ifconfig enp0s3");
+   			$ip = shell_exec("/sbin/ifconfig");
    			echo "<h1><pre>$ip</pre></h1>";
 		?>
 </body>
